93413d878bf6b3ca4191a65d7a995cd66846aed0
Ralph Barton <ralph_barton_@hotmail.com>
2016-02-04 23:55:59 +0000

    new file for Toast code

---
+++ public/snatch_files/snDraw-Game-Toast.js
@@ -0,0 +1,17 @@
+snDraw.Game.Spell = {
+
+    // a note about players[i].saved_x_plotter // TODO write the note...
+    n_toasts: 0,
+    s: 0,
+    n_toasts: 0,
+
+    addToastToQueue: function(){
+
+    },
+    removeToastFromQueue: function(){
+
+    },
+    spareFunction: function(){
+
+    },
+}
