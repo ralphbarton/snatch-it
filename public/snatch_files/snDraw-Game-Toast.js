1a6ae9efbb80cc9803514b5a729f37e67197fdc3
Ralph Barton <ralph_barton_@hotmail.com>
2016-02-05 12:08:22 +0000

    minor additions

--- public/snatch_files/snDraw-Game-Toast.js
+++ public/snatch_files/snDraw-Game-Toast.js
@@ -2,12 +2,15 @@ snDraw.Game.Spell = {
 
     // a note about players[i].saved_x_plotter // TODO write the note...
     n_toasts: 0,
-    s: 0,
-    n_toasts: 0,
+    toast_heights: [],
+    ABC: 0,
 
     addToastToQueue: function(){
 
     },
+    createToastObj: function(){
+	df
+    }
     removeToastFromQueue: function(){
 
     },
