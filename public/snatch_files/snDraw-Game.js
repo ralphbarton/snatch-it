4bc47d2e14922414147a968d11d8902fa296826e
Ralph Barton <ralph_barton_@hotmail.com>
2016-02-18 16:36:40 +0000

    code that draws the player zones split off into a separate object (snDraw.Game.Zones)

--- public/snatch_files/snDraw-Game.js
+++ public/snatch_files/snDraw-Game.js
@@ -20,7 +20,6 @@ snDraw.Game = {
 
     //member variables - dynamic, for rendering...
     unusedTilesBottomPx: undefined,
-    playersZoneTopPx: undefined,
     dark_background: undefined,
     bg_col: undefined,
     fg_col: undefined,
@@ -43,7 +42,7 @@ snDraw.Game = {
 	canvas.clear();	    
 	snDraw.Game.Controls.createControls();//draw buttons on the top of the screen
 	this.createEveryTileObject_inGridAtTop();//next draw all the unturned tiles underneath
-	this.drawEntirePlayerZone();//next draw all the players word zones
+	snDraw.Game.Zones.drawEntirePlayerZone();//next draw all the players word zones
 	canvas.renderAll();
 	TA = this.TileArray;//this is to enable faster debugging
     },
@@ -98,7 +97,7 @@ snDraw.Game = {
 	this.unusedTilesBottomPx = y_plotter + (x_plotter==XPD ? 0 :  tile_space_px + this.tileSize);
 	
 	//it's only upon determining the bottom of the unsed tiles that we can set this variable (and it's done once, right now...)
-	this.playersZoneTopPx = Math.round(this.unusedTilesBottomPx + this.marginUnit);
+	snDraw.Game.Zones.playersZoneTopPx = Math.round(this.unusedTilesBottomPx + this.marginUnit);
     },
 
 
@@ -189,103 +188,6 @@ snDraw.Game = {
 	    myTile.item(0).setFill('yellow');
 	    canvas.renderAll();
 	}
-    },
-
-
-    drawEntirePlayerZone: function(){
-
-	this.calculatePlayerZoneSizes();//this sets attributes within the player objects
-	var plr_top_cumulator = this.playersZoneTopPx;// the starting value for this variable is the lower edge of the tile zone...
-
-	for (var i=0; i<players.length; i++){
-	    players[i].zone_top = plr_top_cumulator;
-	    this.drawPlayerZoneBox(players[i]);// Draws the BOX
-	    this.drawAllPlayerWords(players[i]);//Draws all the WORDS
-	    plr_top_cumulator += players[i].zone_height + this.textMarginUnit + this.stroke_px;
-	}
-    },
-
-
-    calculatePlayerZoneSizes: function(){
-	var nPlayers = players.length;
-	nLettersPlayer = [];
-	nLettersTotPlayed = 0;
-	
-	//count the number of letters each player has, and total letters used within words
-	for(i=0; i<nPlayers; i++){
-	    nLettersPlayer[i] = 0;
-	    for(j=0; j<players[i].words.length; j++){
-		nLettersPlayer[i] += players[i].words[j].length;
-	    }
-	    nLettersTotPlayed += nLettersPlayer[i];
-	}
-
-	//determine total amount of height contained within players' zone boxes
-	section_height = myZoneHeight - this.playersZoneTopPx;
-	zones_sum_height = section_height - nPlayers * this.stroke_px - (nPlayers-1)*this.textMarginUnit - this.marginUnit;
-	basic_height = this.tileSize + 4*this.marginUnit;
-	shareable_height = zones_sum_height - nPlayers * basic_height;
-
-	for(i=0; i<nPlayers; i++){
-	    //now, we don't want to go dividing by zero if it's a new game with nothing played!!
-	    var hRatio = 0;
-	    if(nLettersTotPlayed){
-		hRatio = nLettersPlayer[i]/nLettersTotPlayed;
-	    }else{
-		hRatio = 1 / nPlayers;
-	    }
-
-	    //this line of code adds the attribute calculated to the relevant player object within the array...
-	    players[i].zone_height = basic_height + Math.round( hRatio * shareable_height );
-	}
-    },
-
-
-    drawPlayerZoneBox: function(myplayer){
-
-	var zoneBox = new fabric.Rect({
-	    left: this.marginUnit,
-	    top: myplayer.zone_top,
-	    fill: this.bg_col,
-	    stroke: myplayer.color,
-	    strokeWidth: this.stroke_px,
-	    width: myZoneWidth - 2 * this.marginUnit - this.stroke_px,
-	    height: myplayer.zone_height,
-	});
-
-	var plrName = new fabric.Text(myplayer.name,{
-	    left: 4 * this.marginUnit,
-	    top: myplayer.zone_top - this.textMarginUnit,
-	    fontSize: 2 * this.textMarginUnit,
-	    textBackgroundColor: this.bg_col,
-	    fill: myplayer.color,
-	});
-
-	var plrZone = new fabric.Group([zoneBox,plrName],{
-	    hasControls: false,
-	    hasBorders: false,
-	    lockMovementX: true,
-	    lockMovementY: true
-	});
-
-	canvas.sendToBack(plrZone);
-    },
-
-
-    // for example player.words : [[23,14,11],[44,12,13,19,4]]
-    drawAllPlayerWords: function(myplayer){
-	//since this function is only invoked when first drawing the game for each player, this is the time to set the base coordinates:
-	myplayer.x_next_word = this.x_plotter_R;
-	myplayer.y_next_word = myplayer.zone_top + 1.8 * this.marginUnit;
-	//LOOP thru all the player's words...
-	// draw them onscreen
-	for (var i=0; i<myplayer.words.length; i++){
-	    this.drawSingleCapturedWord(myplayer, i);	
-	}
-
-	//this prep's the SPELL class to place letters in the right location
-	// it is needed within this function call because the player won't necessarily have any words at the start...
-	snDraw.Game.Spell.restoreBasePosition();
     },
 
 
