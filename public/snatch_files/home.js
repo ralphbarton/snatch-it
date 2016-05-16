cb100b1a30c6450481f1ad4bfb43b0517e28880a
Ralph Barton <ralph_barton_@hotmail.com>
2016-05-16 18:47:29 +0100

    I've started constructing a bit of a sketchy HTML website to handle multiple game instances with initiate and Join game features

---
+++ public/snatch_files/home.js
@@ -0,0 +1,17 @@
+function initiate_game(){
+    $("#page1").css("display", "none");
+    $("#page2").css("display", "block");
+};
+
+function join_game(){
+    $("#page1").css("display", "none");
+    $("#page3").css("display", "block");
+
+};
+
+function show_home(){
+    $("#page1").css("display", "block");
+    $("#page2").css("display", "none");
+    $("#page3").css("display", "none");
+
+};
