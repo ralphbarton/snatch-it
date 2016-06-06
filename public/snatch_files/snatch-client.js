abc8970d4879b3e3ea85361102ef05c71ec0d94c
Ralph Barton <ralph_barton_@hotmail.com>
2016-06-06 20:14:52 +0100

    WiP work done on flight to Belfast to split 'home' and 'game' into 2 separate HTML pages for modularisation advantages

--- public/snatch_files/snatch-client.js
+++ public/snatch_files/snatch-client.js
@@ -9,19 +9,25 @@ var client_player_index = undefined;
 
 var dev = false; // select whether development more is active or not
 
-//initialise the Canvas
+/// TO DO: REORGANISE THIS...
+// in the case where the pin is populated in the page that has just downloaded, this means the client must send a message to
+// the server...
+window.onload = function() {
+    var pin = $("#pin").html();
+    if((!isNaN(pin)) && (pin.length==4)){//test it is a 4 digit numeric string...
+	start_game(pin);
+    }
+};
 
+function start_game(room_pin_2){
+    socket.emit('join room and start', (room_pin_2 != undefined ? room_pin_2 : room_pin));
+};
 
 
 //Provides data to allow player to reclaim name, make new name AND choose their color 
 socket.on('player color choices', function(msg_obj){
 
     //code to hide any other 'pages' and show the Canvas that is the snatch game...
-    $("#page1").css("display", "none");
-    $("#page2").css("display", "none");
-    $("#page3").css("display", "none");
-    $("#page4").css("display", "block");
-
     snDraw.initialiseCanvas();
     snDraw.makeCanvasFitWholeWindow();
 
