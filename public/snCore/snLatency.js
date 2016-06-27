facc36bbbf7be647a0891d57b96a71cae10a747d
Ralph Barton <ralph_barton_@hotmail.com>
2016-06-27 19:03:55 +0100

    references corrected for new names. Bug-fix in snLatency (reference errors)

--- public/snCore/snLatency.js
+++ public/snCore/snLatency.js
@@ -9,11 +9,11 @@ snCore.Latency = {
 
     // function writes to global data
     LogTransmit: function(key){
-	MessageLog[key] = {timestamp: new Date,
+	this.MessageLog[key] = {timestamp: new Date,
 			   ack: false};
 
 	//in 2.5 seconds time, check if a response was recieved...
-	setTimeout(this.TestIfAwaitingMSG, 1000 * T_latency_for_popup + 1 );//theshold must be surpassed when checked
+	setTimeout(this.TestIfAwaitingMSG, 1000 * this.T_latency_for_popup + 1 );//theshold must be surpassed when checked
 
 	//Aside from the heartbeat (hb) message, client message transmission is a user-active event. Clear Persistent Toasts.
 	if(key != "hb"){
@@ -28,10 +28,10 @@ snCore.Latency = {
 
 	// in cases where a message was recieved which was not expected (examples would be another player turns/snatches)
 	// take no action with regards to assessing the severity of latency of clearing the latency error message...
-	if(MessageLog[key] != undefined){
+	if(this.MessageLog[key] != undefined){
 	    var L = this.GetTimeCurrentlyWaited();
 	    //This resets the flag which indicates that a message has not been recieved. Must grab latency beforehand...
-	    MessageLog[key].ack = true;
+	    this.MessageLog[key].ack = true;
 
 	    // Clear the "connection lost" message if it is present
 	    if(snCore.Popup.popup_in_foreground == "connection"){
@@ -40,9 +40,9 @@ snCore.Latency = {
 		//comment numerically on the latency...
 		var lat = L.latency;
 		var q_te = "slightly ";
-		if (lat > T_latency_thres2){
+		if (lat > this.T_latency_thres2){
 		    var q_te = "extremely ";
-		}else if(lat > T_latency_thres1){
+		}else if(lat > this.T_latency_thres1){
 		    var q_te = "";
 		}
 
@@ -53,16 +53,16 @@ snCore.Latency = {
 
 
     // function has no side-effects this function was formerly "get_max_latency"
-    GetTimeCurrentlyWaited: function(key){
+    GetTimeCurrentlyWaited: function(){
 
 	var time_now = new Date;
 
 	//for any message that is currently unanswered, how long have we been waiting? Get the max...
 	var max_latency = 0;
 	var max_key = undefined;
-	for (var key in MessageLog) {
-	    if (MessageLog.hasOwnProperty(key)) {
-		var key_latency = MessageLog[key].ack ? 0 : time_now - MessageLog[key].timestamp;
+	for (var key in this.MessageLog) {
+	    if (this.MessageLog.hasOwnProperty(key)) {
+		var key_latency = this.MessageLog[key].ack ? 0 : time_now - this.MessageLog[key].timestamp;
 		max_key = key_latency>max_latency ? key : max_key;
 		max_latency = Math.max(max_latency, key_latency);
 	    }
@@ -74,9 +74,10 @@ snCore.Latency = {
 	}
     },
 
-    TestIfAwaitingMSG: function(key){
+    TestIfAwaitingMSG: function(){
 	//if any message has been waiting for more than 2 seconds, complain
-	if(this.GetTimeCurrentlyWaited().latency >= T_latency_for_popup){//UNITS OF SECONDS...
+	var LLL = snCore.Latency.GetTimeCurrentlyWaited();// On this line, I just don't understand why 'this' had to be 'snCore.Latency'
+	if(LLL.latency >= this.T_latency_for_popup){//UNITS OF SECONDS...
 	    snCore.Popup.openModal("connection");
 	}
     }
