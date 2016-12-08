a7f2d6aca63d0bf010b9145866f816dcb5340670
Ralph Barton <ralph_barton_@hotmail.com>
2016-12-08 17:03:54 +0000

    No functional change. Mild alterations and improvements to comments in this file as I review it

--- public/snCore/snToast.js
+++ public/snCore/snToast.js
@@ -1,7 +1,7 @@
 snCore.Toast = {
 
     ToastCounter: 0,//create an integer_key for each toast
-    ToastRolling: [],//rolling buffer (length up to 3 - tbc) holding the integer_key's for the dictionary above.
+    VisibleToast_keys_list: [],//rolling buffer (length up to 3 - tbc) holding the integer_key's for the dictionary above.
     ToastTop_consumed_words: 0,
     ToastTop_snatched_word: 0,
     ToastTop_client_words_final: 0,
@@ -26,21 +26,22 @@ snCore.Toast = {
 
     showToast: function(my_string, ToastOptions){
 
-	//how do we reference back to this Toast (important in the placement process...)?
+	// 1. Basic creation
+	// 1.1 Create a new unique key for this Toast; increment the counter
 	var t_key = "toast-n" + this.ToastCounter; 
 	this.ToastCounter++;
-	
-	this.ToastRolling.push(t_key); // rolling is a []
+	this.VisibleToast_keys_list.push(t_key);
 
-	//Actually the div of the new Toast class...
+	// 1.2 Add to the DOM
 	var $NewToast = $( "<div/>", {id: t_key}).addClass("ToastGrey ToastCentral ToastIn").html(my_string);
-	
 	$("#canv-inside").append($NewToast);
 
-	var toast_spacing = snCore.Tile.stdDimention * 0.18;
-	//Determine the vertical position of the toast (start it at its highest potential position)
+	// 2. Determine Vertical Placement for new Toast - a two step algorithm
+
+	// 2.1 Step 1 - Decide the upper boundary which all toasts must be beneath
 	var ClientZone_Title = snCore.Zones.PlayerZone[0].Zone_FabObjs[1];
 	var ToastTop_current_client_zone_top = ClientZone_Title.top + ClientZone_Title.height;
+
 	var Client_Words_Grps = snCore.Words.TileGroupsArray[client_player_index];
 	var ToastTop_current_client_words = 0;
         for(var i = 0; i < Client_Words_Grps.length; i++){
@@ -56,39 +57,40 @@ snCore.Toast = {
 				 ToastTop_current_client_zone_top);
 	this.reset_ToastTop_params();
 
-	//Now this bit relates to fitting it around toasts which may already be there...
-	for(var i = 0; i < this.ToastRolling.length; i++){
-	    var t_key_i = this.ToastRolling[i];
+	// 2.2 Step 2 - keep shuffling it down until it does not interfere
+	// i.e. this 'fits it around' all existing toasts which are already there
+	var toast_spacing = snCore.Tile.stdDimention * 0.18;
+	for(var i = 0; i < this.VisibleToast_keys_list.length; i++){
+	    var t_key_i = this.VisibleToast_keys_list[i];
 	    var $ExistingToast = $("#"+t_key_i);
 
-
-	    //trap it in the loop by reverting i to zero until it escapes interference with any other Toasts...
 	    var Toast_i_top = $ExistingToast.position().top;
 	    var Toast_i_height = $ExistingToast.outerHeight();
 	    var Toast_i_bot = Toast_i_top + Toast_i_height + toast_spacing;
 	    var toast_height = $NewToast.outerHeight();
 	    var toast_bot = toast_top + toast_height + toast_spacing;
-	    //a smaller vertical coordinate means higher up on screen...
-	    //is there interference
 
+	    // This -IF- statement tests for Interference
+	    // if found, the looping variable is reset. Thus a clean sweep is required for loop to terminate
 	    if((Toast_i_top <= toast_bot)&&(Toast_i_bot >= toast_top)){//Interference detected
 		toast_top = Toast_i_bot + 0.5;//shuffle down the candidate position
 		i = -1;//reset the loop (it will get imcremented, so must here make it -1.
-	    }else{
-		// huh?
 	    }
 	}
-
+	
+	// 2.3 Now, place the Toast in position
 	$NewToast.css("top", (toast_top + "px"));
 
-	//toasts scaling (there are some more...)
-	//TODO: this styling code which applies to all toasts should be elsewhere
-	// why can't I use jQuery to change the properties of the CSS class universally whilst no objects of that class exist?
+	// 3. Certain styles are dependent on screen scaling. Apply these to the DOM object here.
 	var sd = snCore.Tile.stdDimention;
 	$NewToast.css("font-size", (sd*0.35)+"px");
 	$NewToast.css("-moz-border-radius", (sd*0.1)+"px");
 	$NewToast.css("-webkit-border-radius", (sd*0.1)+"px");
 	$NewToast.css("border-radius", (sd*0.1)+"px");
+	// why can't I use jQuery to change the properties of the CSS class universally whilst no objects of that class exist?
+
+
+	// 4. Apply options to the generated Toast...
 
 	/*
 	ToastOptions = {
@@ -100,7 +102,6 @@ snCore.Toast = {
 	}
 	*/
 
-	// Apply options to the generated Toast...
 	if (ToastOptions == undefined){
 	    //the default behaviour
 	    this.setToastRemTimeout(t_key);
@@ -113,7 +114,7 @@ snCore.Toast = {
 		    snCore.Toast.setToastRemTimeout(t_key, {instant: true});});
 		// it'll close by itself in a minute...
 		this.setToastRemTimeout(t_key, {duration: 60000});
-		// it will get
+		// add it into the persistent Toasts list, also.
 		this.persistent_toast_list_byKey.push(t_key);
 	    }else{
 		// also use this behaviour when not persistent
@@ -128,13 +129,15 @@ snCore.Toast = {
 		$NewToast.html("").append(ToastOptions.HTML_frag);
 	    }
 
+
+	    //Add visuals and logic to the DIV so that click 1 = HOLD and click 2 = Vanish
 	    if(ToastOptions.holdable == true){
-		var via_kb = ToastOptions.via_KB == true;
+		var via_kb = ToastOptions.via_KB == true; // did KB (rather than mouse) activity bring Toast into existance?
 
-		var stop_msg_str = (via_kb?"Hit 6 again":"click") + " to hold in view";
-		var stop_msg = $('<div/>', {id: "m1"+t_key}).addClass("ToastStop S1").text(stop_msg_str);
+		var HoldToast_str = (via_kb?"Hit 6 again":"click") + " to hold in view";
+		var $HoldToast_div = $('<div/>', {id: "m1"+t_key}).addClass("ToastStop S1").text(HoldToast_str);
 
-		//clear the 'hold' message
+		//Make the "Hit 6" text disappear in 1.2 seconds (timeout to alter visible text)
 		if(via_kb){
 		    setTimeout(function(){
 			if($("#m1"+t_key).text().includes("Hit 6")){
@@ -143,13 +146,14 @@ snCore.Toast = {
 		    }, 1200);
 		}
 
-		$NewToast.prepend(stop_msg).click(function(){
+		// Add the callback function to the "hold in view" text, to achieve the HOLD functionality.
+		$NewToast.prepend($HoldToast_div).click(function(){
 		    snCore.Toast.holdToast(t_key, via_kb);
 		});
 	    }
 	}
 
-	//any <a href> into new windows... (this will apply to links in str or frags...)
+	// 5. Cause links within Toast Text to open in a new window (this will apply to links in str or frags...)
 	$NewToast.find( "a" ).attr("target","_blank");
 	$NewToast.find( ".samewin" ).attr("target","_self");
 
@@ -197,9 +201,9 @@ snCore.Toast = {
 	    setTimeout(function(){
 
 		//find the array index of the toast concerned (by its key)
-		var roll_index = snCore.Toast.ToastRolling.indexOf(t_key);
+		var kill_index = snCore.Toast.VisibleToast_keys_list.indexOf(t_key);
 		//remove this toast key from the array: it is GONE!
-		snCore.Toast.ToastRolling.splice(roll_index, 1);
+		snCore.Toast.VisibleToast_keys_list.splice(kill_index, 1);
 		$("#"+t_key).remove();
 		snCore.Toast.Active_byKey[t_key] = false;
 
